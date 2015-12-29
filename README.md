1a80fc36de43924915deb28fb24f2e88e3b5c9f8
Fabugo <fabugo@gmail.com>
2015-12-29 13:59:04 -0300

    now for windows and check files before execute

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Simple software for generate certificates for your events.
 
 How use
 ========
+Create your SVG file containing '___NAME___' where you want the name to be added.
+Then use:
 ```bash
 python g_certificates.py svg_file list_of_names folder
 ```
@@ -18,7 +20,7 @@ python g_certificates.py example participants certificates
 
 Requirements
 ========
-1 - Any Linux, Unix or MacOS operational system.
+1 - Linux, Unix, MacOS and Windows operational system.
 
 2 - Inkscape installed.
 
@@ -26,3 +28,7 @@ Screenshot
 ========
 
 ![screenshot](http://i.imgur.com/O9yDYGF.png)
+
+Tip
+========
+in case of problems with text, transform text into curves/lines except the '___NAME___'
--- g_certificates.py
+++ g_certificates.py
@@ -5,52 +5,71 @@ import sys
 import re
 import csv
 
-
 # Generate pdf file
-def generate_pdf(filename, directory):
-    print "    generating pdf certificate"
+def export_pdf(filename, directory):
+    print "    exporting pdf certificate from "+directory+"/"+filename+".pdf"
     command = 'inkscape %s.svg --export-pdf=%s/%s.pdf' % (filename, directory, filename)
     os.system(command)
-    print "    removing svg file"
-    remover = 'rm %s.svg' % (filename)
-    os.system(remover)
+    print "    removing svg file\n"
+    if sys.platform == "win32": # Windows mode
+        remove = 'del %s.svg' % (filename)
+    else:                       # Unix mode
+        remove = 'rm %s.svg' % (filename)
+    os.system(remove)
 
-# Generate svg file
-def generate_svg(svg_name, svg_file, filename, name):
+# Modifies svg file copy
+def modify_svg(svg_name, svg_file, filename, name):
     print "    copying the svg file from " + filename + ".svg"
-    command = 'cp %s.svg %s.svg' % (svg_name, filename)
+    if sys.platform == "win32": # Windows mode
+        command = 'copy %s.svg %s.svg' % (svg_name, filename)
+    else:                       # Unix mode
+        command = 'cp %s.svg %s.svg' % (svg_name, filename)
     os.system(command)
-    
+
+    # Including name
     new_certificate = open(filename + ".svg",'r+')
-    new_certificate.write( re.sub("___NAME___", name, svg_file) )
+    new_certificate.write(re.sub("___NAME___", name, svg_file))
     new_certificate.close()
 
 # Generate the certificates
-def generate_certificates(svg_name, svg_file, list_names_file, directory):
+def generate_certificates(svg_name, list_names_file, directory):
+    svg_file = open(svg_name+".svg").read()  # Open and read the SVG file
     list_names = open(list_names_file)
-    
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    
     for name in list_names:
         name = name.rstrip("\n")
-        
-	if (name):
-	    print "Generating " + name + "'s certificate:\n"
+        if (name):
+            print "Generating " + name + "'s certificate:\n"
             filename = name.replace(" ","_")
+            modify_svg(svg_name, svg_file, filename, name)
+            export_pdf(filename, directory)
+            print "Finished\n\n"
 
-	    generate_svg(svg_name, svg_file, filename, name)
-            generate_pdf(filename, directory)
-
+# Main
 if __name__ == '__main__':
     if(len(sys.argv) != 4):
-        print "Usage: python g_certificates.py <svg_file> <list_of_names> <folder>"
+        print "Usage: python g_certificates.py <svg_name> <list_of_names> <folder>"
         sys.exit()
-    
-    svg_name = sys.argv[1]  # SVG file
+
+    svg_name = sys.argv[1]  # Name of svg file
     list_names_file = sys.argv[2]  # List of names for generate the certificates
     directory = sys.argv[3]  # Directory for storage the certificates
 
-    svg_file = open(svg_name+".svg").read()  # Open and read the SVG file
+    # Check files
+    if not os.path.isfile(svg_name+".svg"):
+        print "The "+svg_name+".svg file not found"
+        sys.exit()
+    if not os.path.isfile(list_names_file):
+        print "The "+list_names_file+" file not found"
+        sys.exit()
 
-    generate_certificates(svg_name, svg_file, list_names_file, directory)
+    # Check directory
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Check platform
+    if sys.platform == "win32":
+        print "Windows mode"
+    else:
+        print "Unix mode"
+
+    generate_certificates(svg_name, list_names_file, directory)
